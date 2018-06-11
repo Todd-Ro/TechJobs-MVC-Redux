9504d4fd207643927596370cae5f04ece09eb10a
Todd Romkema <todd.romkema@gmail.com>
2018-06-10 20:52:15 -0500

    Initial commit - Reached the point with setting up the initial code where the index page works properly

---
+++ src/main/java/org.launchcode/TechJobsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class TechJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechJobsApplication.class, args);
+    }
+}
